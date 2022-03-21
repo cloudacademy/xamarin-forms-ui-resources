15ac0181acb1a3a219700f0b9379a7d6b3022940
Hallam Webber <hallam@how.co.nz>
2022-03-22 09:15:35 +1300

    Add project files.

---
+++ Xaml_UI/Xaml_UI/Views/GridLayoutView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xaml_UI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GridLayoutView : ContentView
+    {
+        public GridLayoutView()
+        {
+            InitializeComponent();
+        }
+    }
+}
